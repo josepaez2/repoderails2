ede91027cc161112e31a0bf66f7de4f2b3056253
josepaez2 <josepaez_2@hotmail.com>
2017-07-25 09:49:44 -0500

    final changes

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'movies/edit'
+
+  get 'movies/destroy'
+
+  get 'movies/new'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
